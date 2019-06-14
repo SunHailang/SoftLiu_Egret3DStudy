250f1381a3eeef6a50379dd5d112080557a61944
SunHailang <hai-lang.sun@ubisoft.com>
2019-06-14 16:07:00 +0800

    update project add some res

--- Clone/src/Main.ts
+++ Clone/src/Main.ts
@@ -1,5 +1,16 @@
+/**
+ *  __author__ = "sun hai lang"
+ *  __date__ = 2019-06-14
+ * 
+ */
+
+import Splash from "./Splash/Splash";
+
 async function main() {
     await RES.loadConfig("resource/default.res.json", "resource/");
     await RES.getResAsync('Asset/scenes/newScene.scene.json');
     paper.Application.sceneManager.loadScene('Asset/scenes/newScene.scene.json');
+
+    // 强制引用，该问题将在 Egret Pro 1.0.0 修复。
+    Splash;
 }
---
+++ Clone/src/Splash/Splash.ts
@@ -0,0 +1,43 @@
+export default class CubeMove extends paper.Behaviour
+{
+    onAwake()
+    {
+
+    }
+
+    onEnable()
+    {
+
+    }
+
+    onDisable() 
+    {
+
+    }
+
+    onStart()
+    {
+
+    }
+
+    onUpdate(delta:number)
+    {
+
+    }
+
+    onFixedUpdate(delta:number)
+    {
+
+    }
+
+    onLateUpdate(delta:number)
+    {
+
+    }
+
+    onDestroy()
+    {
+
+    }
+
+}
